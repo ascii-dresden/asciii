62ad3ae21edf06fd9a9802ace2f2c7173074bf3e
Hendrik Sollich <hendrik@hoodie.de>
2016-04-14 19:38:55 +0200

    latest doc

--- implementors/git2/trait.IntoCString.js
+++ implementors/git2/trait.IntoCString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['git2'] = [];implementors['git2'] = [];
+implementors['git2'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
