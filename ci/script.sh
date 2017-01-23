11dc9ddcc53ef7c8f5a6abe850eea6100afa4d00
Mathias Kaufmann <me@stei.gr>
2017-01-23 22:23:30 +0100

    Just compile
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- ci/script.sh
+++ ci/script.sh
@@ -2,23 +2,21 @@
 
 set -ex
 
-export RUST_BACKTRACE=1
-
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     ./build.sh
-    cross build --target $TARGET
+    # cross build --target $TARGET
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    # cross test --target $TARGET
+    # cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
